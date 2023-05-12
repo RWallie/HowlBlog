87a5fc8ca65c75a1c829e3aa2fa7be379b948461
Ryan <ryanwallace1396@gmail.com>
2023-05-12 14:06:06 -0700

    changed BlogRepository to JpaRepository for more functionality from PagingAndSorting and Crud Repos

--- src/main/java/com/howl/blog/repository/BlogRepository.java
+++ src/main/java/com/howl/blog/repository/BlogRepository.java
@@ -1,11 +1,9 @@
 package com.howl.blog.repository;
 
 import com.howl.blog.model.Blog;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public interface BlogRepository extends CrudRepository<Blog, Long>{
+public interface BlogRepository extends JpaRepository<Blog, Long>{
   
 }
