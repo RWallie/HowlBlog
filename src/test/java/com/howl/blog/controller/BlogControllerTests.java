8973b3e2329f71e3638c4400a37eb686c19e504b
Ryan <ryanwallace1396@gmail.com>
2023-05-13 12:18:34 -0700

    created integration tests for CRUD operations at controller endpoints

--- src/test/java/com/howl/blog/controller/BlogControllerTests.java
+++ src/test/java/com/howl/blog/controller/BlogControllerTests.java
@@ -2,13 +2,18 @@ package com.howl.blog.controller;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.Arrays;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,11 +22,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.howl.blog.dto.BlogDto;
-// import com.howl.blog.model.Blog;
+import com.howl.blog.dto.BlogResponse;
 import com.howl.blog.service.BlogService;
 
 @WebMvcTest(BlogController.class)
@@ -38,7 +44,6 @@ public class BlogControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    // private Blog blog;
     private BlogDto blogDto;
 
     @BeforeEach
@@ -46,15 +51,6 @@ public class BlogControllerTests {
         long millis = System.currentTimeMillis();
         Date date = new Date(millis);
         Time time = new Time(millis);
-
-        // blog = Blog.builder()
-        //     .title("Rock Climbing Grip Strength")
-        //     .author("Ryan Wallace")
-        //     .category("Sports")
-        //     .text("Lorem ipsum dolor sit amet")
-        //     .publishDate(date)
-        //     .publishTime(time)
-        //     .build();
 
         blogDto = BlogDto.builder()
             .title("Rock Climbing Grip Strength")
@@ -67,7 +63,7 @@ public class BlogControllerTests {
     }
 
     @Test
-    public void addBlogPostTest1() throws Exception {        
+    public void addBlogPostTest() throws Exception {        
         given(blogService.addBlog(ArgumentMatchers.any()))
             .willAnswer((invocation -> invocation.getArgument(0)));
 
@@ -75,6 +71,82 @@ public class BlogControllerTests {
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(blogDto)));
 
-        response.andExpect(MockMvcResultMatchers.status().isCreated());
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title", CoreMatchers.is(blogDto.getTitle())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.author", CoreMatchers.is(blogDto.getAuthor())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.category", CoreMatchers.is(blogDto.getCategory())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.text", CoreMatchers.is(blogDto.getText())))
+            .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void getAllBlogsTest() throws Exception {    
+        BlogResponse responseDto = BlogResponse.builder()
+            .blogs(Arrays.asList(blogDto))
+            .build();
+
+        when(blogService.getAllBlogs()).thenReturn(responseDto.getBlogs());
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/blogs"));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.length()", CoreMatchers.is(responseDto.getBlogs().size())))
+            .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void getPokemoneByIDTest() throws Exception {
+        long blogId = 1;
+        when(blogService.getBlogById(blogId)).thenReturn(blogDto);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/blogs/1")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(blogDto)));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title", CoreMatchers.is(blogDto.getTitle())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.author", CoreMatchers.is(blogDto.getAuthor())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.category", CoreMatchers.is(blogDto.getCategory())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.text", CoreMatchers.is(blogDto.getText())))
+            .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void updateBlogPostByIdTest() throws Exception {     
+
+        long blogId = 1;
+
+        BlogDto updateBlogDto = BlogDto.builder()
+            .title("updated title")
+            .author("updated author")
+            .category("updated category")
+            .text("updated text")
+            .build();        
+
+        when(blogService.updateBlogById(blogId, updateBlogDto)).thenReturn(updateBlogDto);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.put("/blogs/1")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(updateBlogDto)));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title", CoreMatchers.is(updateBlogDto.getTitle())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.author", CoreMatchers.is(updateBlogDto.getAuthor())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.category", CoreMatchers.is(updateBlogDto.getCategory())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.text", CoreMatchers.is(updateBlogDto.getText())))
+            .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void deleteBlogByIdTest() throws Exception {
+        long blogId = 1;
+
+        doNothing().when(blogService).deleteBlogById(blogId);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.delete("/blogs/1")
+            .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+            .andDo(MockMvcResultHandlers.print());
     }
 }
